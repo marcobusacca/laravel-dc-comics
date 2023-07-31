be870775dc03eb1ef4596a14cf413dc06a9e1fb5
marcobusacca <marcobusacca01@gmail.com>
2023-07-31 16:53:54 +0200

    [MILESTONE-2] create button in comics index page

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -4,6 +4,9 @@
     <div class="index-page">
         <div class="container py-5">
             <div class="row">
+                <div class="col-12 text-center">
+                    <a href="{{ Route('comics.create') }}" class="btn btn-success">Aggiungi Comic</a>
+                </div>
                 @forelse ($comics as $comic)
                     <div class="col-12 col-md-6 col-lg-4 my-5">
                         <div class="card h-100">
