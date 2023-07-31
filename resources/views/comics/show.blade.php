7de114472da8e4819c40b0106a7adb14d09cdc87
marcobusacca <marcobusacca01@gmail.com>
2023-07-31 17:37:41 +0200

    [MILESTONE-2] comics create page layout

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+    <div class="container py-5">
         <div class="row">
-            <div class="col-12 text-center mt-4">
+            <div class="col-12 text-center">
                 <a href="{{ Route('comics.index') }}" class="btn btn-dark">Homepage</a>
             </div>
-            <div class="col-12 mt-4 mb-5">
+            <div class="col-12 my-5">
                 <div class="card">
                     <!-- Card Header -->
                     <div class="card-header d-flex justify-content-center align-items-center text-center">
@@ -17,11 +17,11 @@
                     <div class="card-body">
                         <div class="row">
                             <!-- Image -->
-                            <div class="col-4 d-flex justify-content-center">
+                            <div class="col-12 col-lg-4 d-flex justify-content-center">
                                 <img src="{{ $comic->thumb }}" alt="banner-image" class="border border-black border-5">
                             </div>
                             <!-- Details -->
-                            <div class="col-8 d-flex flex-wrap align-items-center">
+                            <div class="col-12 col-lg-8 d-flex flex-wrap align-items-center">
                                 <!-- Description -->
                                 <div>
                                     <h3 class="fw-bold py-2">Description</h3>
