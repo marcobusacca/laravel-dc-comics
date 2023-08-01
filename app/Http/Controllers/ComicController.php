344c454d01ebfbcefc904f30fafcbda6d90bfb76
marcobusacca <marcobusacca01@gmail.com>
2023-08-01 14:49:13 +0200

    [FEAT] comic-controller function edit

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -82,7 +82,7 @@ class ComicController extends Controller
      */
     public function edit(Comic $comic)
     {
-        //
+        return view('comics.edit', compact('comic'));
     }
 
     /**
